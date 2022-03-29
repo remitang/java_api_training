b1e087259c35b6a6d1c30e132d2f1042a1a7e7c9
Remi Tang <remi.tang@epita.fr>
2022-03-29 22:49:22 +0200

    Add Game test to check CheckTarget functionalities

--- src/test/java/fr/lernejo/navy_battle/GameTest.java
+++ src/test/java/fr/lernejo/navy_battle/GameTest.java
@@ -5,13 +5,33 @@ import org.junit.jupiter.api.Test;
 
 public class GameTest {
 
-    int[] boatSize = {1};
-    String[][] boatCoordinates = {{"A3"}};
+    int[] boatSize = {1, 2, 1, 1, 1};
+    String[][] boatCoordinates = {{"A1"}, {"A2", "A3"}, {"A4"}, {"A5"}, {"A6"}};
     Game game = new Game(boatSize, boatCoordinates);
 
     @Test
     public void gameNextAttack() {
         game.nextAttack();
         Assertions.assertEquals(game.attack[0], 1);
+    }
+
+    @Test
+    public void gameShipLeft() {
+        Assertions.assertTrue(game.checkShipLeft());
+    }
+
+    @Test
+    public void gameCheckTargetMiss() {
+        Assertions.assertEquals(game.checkTarget("B2"), "miss");
+    }
+
+    @Test
+    public void gameCheckTargetHit() {
+        Assertions.assertEquals(game.checkTarget("A2"), "hit");
+    }
+
+    @Test
+    public void gameCheckTargetSunk() {
+        Assertions.assertEquals(game.checkTarget("A1"), "sunk");
     }
 }
